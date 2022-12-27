dbab9dafe7ae87baed55e0edb0af19c932d21dde
Sarvesh326 <57324641+Sarvesh326@users.noreply.github.com>
2022-12-27 23:22:59 +0530

    Update app.py

--- app.py
+++ app.py
@@ -46,7 +46,7 @@ def predict_sentiment(data:Review):
 
 # 5. Run the API with uvicorn
 #    Will run on http://127.0.0.1:8000
-if __name__ == '__main__':
-    uvicorn.run(app, host='127.0.0.1', port=8000)
+# if __name__ == '__main__':
+#    uvicorn.run(app, host='127.0.0.1', port=8000)
     
 #uvicorn app:app --reload
